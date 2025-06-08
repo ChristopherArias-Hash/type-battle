673a8a4d7319b64cba583cd69addd0a73146dbfd
Chris Arias <144634991+ChristopherArias-Hash@users.noreply.github.com>
2025-06-08 01:06:44 -0700

    test comment

--- type-battle/src/main/java/com/example/type_battle/TypeBattleApplication.java
+++ type-battle/src/main/java/com/example/type_battle/TypeBattleApplication.java
@@ -2,7 +2,7 @@ package com.example.type_battle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class TypeBattleApplication {
 
