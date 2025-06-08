69324553f1094d5094b7eb300763e8bd275411a4
Chris Arias <144634991+ChristopherArias-Hash@users.noreply.github.com>
2025-06-07 19:41:47 -0700

    Moved files to correct folders

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import './MainPage.css'
-import NavBar from './components/NavBar.jsx'
-import JoinMatch from './components/JoinMatch.jsx'
-import CreateMatch from './components/CreateMatch.jsx'
-import LeaderBoard from './components/LeaderBoard.jsx'
-import AboutUs from './components/AboutUs.jsx'
+import NavBar from '../components/NavBar.jsx'
+import JoinMatch from '../components/JoinMatch.jsx'
+import CreateMatch from '../components/CreateMatch.jsx'
+import LeaderBoard from '../components/LeaderBoard.jsx'
+import AboutUs from '../components/AboutUs.jsx'
 
 function MainPage() {
   const [count, setCount] = useState(0)
