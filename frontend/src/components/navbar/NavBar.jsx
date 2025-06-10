531a09065697215b2ddf69cf76c3bdf3590b010a
Chris Arias <144634991+ChristopherArias-Hash@users.noreply.github.com>
2025-06-10 01:30:20 -0700

    Moved navbar items to only show if logged in

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -8,14 +8,19 @@ function NavBar({onLoginClick, onRegisterClick, isUserLoggedIn, logOut}) {
         <ul className="nav-bar">
             <li className="nav-item pull-left"><h1>TYPE BATTLE</h1></li>
             <li className="nav-item pull-right"><img src="https://hourglassmc.com/wp-content/uploads/2016/10/mystery-man.png"/></li>
-            {isUserLoggedIn ? <li className="nav-item pull-right"><a onClick={logOut}>Logout</a></li> : ( <>
+            {isUserLoggedIn ?    
+            <>
+            <li className="nav-item pull-right">WPMA: </li>
+            <li className="nav-item pull-right">AVG:</li>
+            <li className="nav-item pull-right">WINS:</li>
+            <li className="nav-item pull-right"><button onClick={logOut}>Logout</button></li>
+            </> : 
+                    <>
             <li className="nav-item pull-right"><a href="#" onClick={(e) => { e.preventDefault(); onLoginClick(); }}>
                 Login 
             </a></li> 
-            <li className="nav-item pull-right"><a href= "#" onClick={(e) => {e.preventDefault(); onRegisterClick()}}>Register</a></li> </>)} 
-            <li className="nav-item pull-right"><a href = "https://www.atom.com/">WPMA</a></li>
-            <li className="nav-item pull-right"><a href = "https://www.atom.com/">AVG</a></li>
-            <li className="nav-item pull-right"><a href = "https://www.atom.com/">WINS</a></li>
+            <li className="nav-item pull-right"><a href= "#" onClick={(e) => {e.preventDefault(); onRegisterClick()}}>Register</a></li> </>} 
+          
         </ul>
         </>
     )
