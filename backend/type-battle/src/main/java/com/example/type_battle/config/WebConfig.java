59ee68a5e2168db504cd591862d1c01a76bf0cff
Chris Arias <144634991+ChristopherArias-Hash@users.noreply.github.com>
2025-07-27 00:57:40 -0700

    backend prod push test

--- backend/type-battle/src/main/java/com/example/type_battle/config/WebConfig.java
+++ backend/type-battle/src/main/java/com/example/type_battle/config/WebConfig.java
@@ -3,14 +3,13 @@ package com.example.type_battle.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // apply to all endpoints
-                .allowedOrigins("http://localhost:5173") // allow your frontend
+                .allowedOrigins("http://localhost:5173", "https://type-battle.org") // Add your production frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // optional: if you use cookies/session
