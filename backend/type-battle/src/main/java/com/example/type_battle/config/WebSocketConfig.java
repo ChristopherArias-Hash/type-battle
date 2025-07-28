3dc5de5a08a7ccceecafa2d4771f915b83aa6483
Chris Arias <144634991+ChristopherArias-Hash@users.noreply.github.com>
2025-07-27 21:17:21 -0700

    edited website connection name again

--- backend/type-battle/src/main/java/com/example/type_battle/config/WebSocketConfig.java
+++ backend/type-battle/src/main/java/com/example/type_battle/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173", "https://typebattle.org") // Add your production frontend URL
+                .setAllowedOrigins("http://localhost:5173", "https://typebattle.org", "https://type-battle-9cjw.onrender.com") // Add your production frontend URL
                 .withSockJS();
     }
     @Override
