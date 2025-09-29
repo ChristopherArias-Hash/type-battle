504aa6b8e4406e8db8901fd05e5132791d01dc59
Chris Arias <144634991+ChristopherArias-Hash@users.noreply.github.com>
2025-09-29 13:07:12 -0700

    Backend: Crossy road pos update

---
+++ backend/type-battle/src/main/java/com/example/type_battle/DTO/CrossyRoadPositionData.java
@@ -0,0 +1,39 @@
+package com.example.type_battle.DTO;
+
+public class CrossyRoadPositionData {
+    private String uid;
+    private int x;
+    private int y;
+
+    public CrossyRoadPositionData(){
+
+    }
+    CrossyRoadPositionData(String uid, int x, int y) {
+        this.uid = uid;
+        this.x = x;
+        this.y = y;
+    }
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
